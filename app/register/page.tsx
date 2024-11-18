e38c109a33e0041fb654582ec4a6ae05ad55664c
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-18 20:10:59 +0100

    Create page.tsx

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RegisterForm } from "./components/register-form";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+}
