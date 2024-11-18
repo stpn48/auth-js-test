c1d08839b282faa07a95d1a9812780268a8539a3
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-18 20:10:58 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import { RegisterForm } from "@/components/register-form";
+import { auth, signOut } from "@/auth";
+import Link from "next/link";
+import { SwitchThemeButton } from "./components/switch-theme-button";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <RegisterForm />
+    <div className="flex flex-col gap-10">
+      <h1>Hello {session?.user?.email || "Guest"}</h1>
+      <button
+        onClick={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        Sign Out
+      </button>
+      <Link href={"/login"}>login page</Link>
+      <SwitchThemeButton />
     </div>
   );
 }
