587431216686a3304f433bf1eefb27ac3ffcf64c
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-18 20:10:59 +0100

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,26 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import { getUserFromDb } from "./app/actions/auth/get-user-from-db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Credentials({ credentials: { email: {}, password: {} } })],
+  session: {
+    strategy: "jwt", // This avoids storing session data in cookies directly
+  },
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: { email: {}, password: {} },
+      authorize: async (credentials) => {
+        let user = null;
+
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        user = await getUserFromDb(email, password);
+
+        return user || null;
+      },
+    }),
+  ],
 });
