c83be77b4bbaa9edb94993a847b3d0d05c76cf31
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-18 20:10:57 +0100

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoginForm } from "./components/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
